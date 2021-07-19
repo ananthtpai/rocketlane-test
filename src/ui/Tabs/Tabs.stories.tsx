949e349f173383f11d94d7e6da467ab4be809cc8
Ananth T Pai <ananthtpai@gmail.com>
2021-07-19 19:09:05 +0530

    Tabs component

---
+++ src/ui/Tabs/Tabs.stories.tsx
@@ -0,0 +1,29 @@
+import { action } from '@storybook/addon-actions'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import Tabs, { TabPane } from '.'
+
+export default {
+  title: 'UI/Tabs',
+  component: Tabs
+}
+
+export const Normal = () => {
+  const [ activeTabKey, setActiveTabKey ] = useState('all')
+
+  const handleTabChange = (tabKey: string) => {
+    setActiveTabKey(tabKey)
+  }
+
+  return <Tabs activeKey={activeTabKey} onChange={handleTabChange} >
+    <TabPane tab='All' tabKey='all'>
+      Test All
+    </TabPane>
+    <TabPane tab={`❤️·${1}`} tabKey='❤️'>
+      Test ❤️
+    </TabPane>
+    <TabPane tab={`👍·${1}`} tabKey='👍'>
+      Test ❤️
+    </TabPane>
+  </Tabs>
+}
---
+++ src/ui/Tabs/index.tsx
@@ -0,0 +1,76 @@
+import React, { ReactElement, ReactNode } from 'react'
+import styled from 'styled-components'
+
+interface TabPaneProps {
+  tab: string,
+  tabKey: string
+}
+
+export const TabPane:React.FC<TabPaneProps> = ({tabKey, tab, children}) => {
+  return <div style={{padding: '0 16px'}}>
+    {children}
+  </div> 
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const TabNamesList = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0;
+`
+
+interface TabNamesListItemProps {
+  active: boolean
+}
+const TabNamesListItem = styled.li<TabNamesListItemProps>`
+  list-style: none;
+  display: flex;
+  padding: 16px;
+  border-bottom: 2px solid ${props => props.active ? '#0f62fe': '#e0e0e0'};
+  cursor: pointer;
+  line-height: 1.2;
+`
+
+interface Props {
+  children: ReactElement<TabPaneProps>[],
+  activeKey: string,
+  onChange: (tabKey: string) => void
+}
+
+export const Tabs:React.FC<Props> = ({children, activeKey, onChange}) => {
+
+  const handleOnTabClick = (tabKey: string) => {
+    onChange(tabKey)
+  }
+
+  const activeTab = children.find((item, index) => item.props.tabKey === activeKey)
+  return (
+    <Container>
+      <TabNamesList>
+        {
+          children.map((item, index) => {
+            return <TabNamesListItem 
+              key={index} 
+              active={item.props.tabKey === activeKey}
+              onClick={handleOnTabClick.bind(this, item.props.tabKey)}
+            >
+                {item.props.tab}
+            </TabNamesListItem>
+          })
+        }
+      </TabNamesList>
+      <div>
+        {
+          activeTab
+        }
+      </div>
+    </Container>
+  )
+}
+
+export default Tabs
